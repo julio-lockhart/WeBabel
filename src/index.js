2063d6350c7179e978f2c1946496f8674b29ecbd
Julio Lockhart <julio.lockhart16@gmail.com>
2018-03-21 19:38:34 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import App from './components/App';
+
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
