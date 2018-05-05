6c03d3f7a1ac8cba9b20cbea01e8e3f7caca98d5
Julio Lockhart <julio.lockhart16@gmail.com>
2018-05-04 21:58:04 -0400

    Added searching for Gifs

--- src/components/Chatkit/CreateMessageInput/index.js
+++ src/components/Chatkit/CreateMessageInput/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Form, InputGroup, InputGroupAddon, Input, Button } from "reactstrap";
+import Modal from 'react-responsive-modal';
 import styled from "styled-components";
+
+import GiphySelect from 'react-giphy-select';
+import 'react-giphy-select/lib/styles.css';
 
 const Container = styled.div`
   border-top: 1px solid #e0e0e0;
@@ -22,15 +26,43 @@ const Container = styled.div`
   }
 `;
 
+const GifSection = styled.div`
+  margin-bottom: 8px;
+`
+
 class CreateMessageInput extends React.Component {
    constructor(props) {
       super(props);
 
       this.state = {
+         open: false,
+         gifLink: null,
          message: ""
       };
 
       this.onSubmit = this.onSubmit.bind(this);
+   }
+
+   onOpenModal = () => {
+      this.setState({ open: true });
+   };
+
+   onCloseModal = () => {
+      this.setState({ open: false });
+   };
+
+   onGifEntrySelection = (gifLink) => {
+      this.setState({ gifLink: gifLink.url });
+   }
+
+   onGifSelect = () => {
+      const { gifLink } = this.state;
+
+      if (gifLink) {
+         this.setState({ message: gifLink });
+      }
+
+      this.onCloseModal();
    }
 
    onMessageChange = event => {
@@ -60,6 +92,7 @@ class CreateMessageInput extends React.Component {
    render() {
 
       const { user, room } = this.props;
+      const { open } = this.state;
 
       return (
          <Container>
@@ -70,12 +103,28 @@ class CreateMessageInput extends React.Component {
                      type="text"
                      onInput={e => user.isTypingIn({ roomId: room.id })}
                      value={this.state.message}
-                     onChange={this.onMessageChange}
-                  />
+                     onChange={this.onMessageChange} />
+
                   <InputGroupAddon addonType="prepend">
+                     <Button onClick={this.onOpenModal}>
+                        Gif
+                     </Button>
+
                      <Button color="primary" type="submit">
                         Submit
                      </Button>
+
+                     <Modal open={open} onClose={this.onCloseModal} center>
+                        <div className="h2">Search for a Gif</div>
+                        <GifSection>
+                           <GiphySelect onEntrySelect={this.onGifEntrySelection} />
+                        </GifSection>
+
+                        <Button
+                           color="primary"
+                           onClick={this.onGifSelect}
+                           disabled={!this.state.gifLink}>Select</Button>
+                     </Modal>
                   </InputGroupAddon>
                </InputGroup>
             </Form>
