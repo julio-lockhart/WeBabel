22d910c14aebf406a9603078583d41501328cec8
Julio Lockhart <julio.lockhart16@gmail.com>
2018-05-10 23:29:34 -0400

    Changing localhost to https://webabel.herokuapp.com/

--- src/constants/apiInstance.js
+++ src/constants/apiInstance.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { TOKEN_PROVIDER_URL } from '../constants/constants';
 
 export const apiInstance = axios.create({
-    baseURL: "http://localhost:3001/",
-    timeout: 1000,
+   baseURL: "https://webabel.herokuapp.com/:3001/",
+   timeout: 1000,
 });
 
 export const tokenProviderInstance = axios.create({
-    baseURL: TOKEN_PROVIDER_URL,
-    timeout: 1000,
+   baseURL: TOKEN_PROVIDER_URL,
+   timeout: 1000,
 });
